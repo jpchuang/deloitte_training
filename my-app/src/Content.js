0ff7e9a96c4b7ad96b220dbfb579a6e15d81179b
Huang <joehuang@deloitte.nl>
2018-08-30 10:36:12 +0200

    Hello World

---
+++ my-app/src/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="App">
+        end it word de content.
+
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ my-app/src/Test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="App">
+        Dit is het menu
+
+      </div>
+    );
+  }
+}
+
+export default Test;
