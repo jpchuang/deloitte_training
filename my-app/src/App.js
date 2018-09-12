5bed96fdc7afe956694b291c59f70d81a702754d
Huang <joehuang@deloitte.nl>
2018-09-12 16:57:43 +0200

    new layout and composition of components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Test from './Test';
-import Content from './Content';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+import "font-awesome/css/font-awesome.css";
+
+import { HeroComponent } from "./components/HeroComponent/HeroComponent";
+import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { navBarShrink: "" };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+    this.handleScroll();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const domNode = ReactDOM.findDOMNode(this.navEl);
+    const nbs = window.pageYOffset > 100 ? "navbar-shrink" : "";
+    this.setState({ navBarShrink: nbs });
+  }
+
   render() {
+    const nbs = this.state ? this.state.navBarShrink : "";
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Programmeren is leuk
-        </p>
-        <Test/>
-        <Content/>
+      <div>
+        <NavBarComponent navBarShrink={nbs} />
+        <HeroComponent />
       </div>
     );
   }
